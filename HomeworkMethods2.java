b4b0cf8e77e6c1ed9a3c7a8d9b7b0a2e8fa7f841
vlstam <100720483+vlstam@users.noreply.github.com>
2022-04-20 20:34:03 +0300

    Update HomeworkMethods2.java

--- HomeworkMethods2.java
+++ HomeworkMethods2.java
@@ -1,3 +1,10 @@
+/**
+ * Да напишем програма за хвърляне на зарове, като
+ * използваме Math.random();
+ * Може да имаме няколко играчи, като всеки от тях
+ * „хвърля“ 2 зара, печели този играч, които изкара
+ * възможно най-високият сбор от двата зара.
+ */
 package homework;
 
 import java.util.Scanner;
@@ -6,10 +13,14 @@ public class HomeworkMethods2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter how many sides would you like your die to have and how many dice would you like to roll?");
-        for (int a = 0; a <= 1; a++) {
-            System.out.println("Second player, please enter the same parameters: ");
-            System.out.println(diceMethod(scanner));
+        int dice1 = diceMethod(scanner);
+        int dice2 = diceMethod(scanner);
+        System.out.println(dice1 + " " + dice2);
+        if(dice1 > dice2){
+            System.out.println("Player 1 wins!");
 
+        } else{
+            System.out.println("Player 2 winds!");
         }
 
         //System.out.println(diceMethod(6));
@@ -18,11 +29,9 @@ public class HomeworkMethods2 {
 
     static int diceMethod(Scanner scanner) {
         int diceSide = scanner.nextInt();
-        int dice = scanner.nextInt();
-        return (int) ((Math.random() * diceSide + 1) * dice);
+        return (int) ((Math.random() * diceSide + 1) + (Math.random() * diceSide + 1));
 
 
         //return (int) ((Math.random() * dice + 1));
     }
 }
-
